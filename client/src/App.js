c0975fd50daaf444dddedca5bdd9cbf7edb85965
jhorn00 <dawshorn@gmail.com>
2021-03-11 15:20:02 -0500

    working navbar - single file

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
-
-import './App.css';
-import React from 'react';
-import {Route, BrowserRouter} from 'react-router-dom';
-import Home from './Home';
-import Test from './Test';
+import "./App.css";
+import React from "react";
+import { Route, BrowserRouter } from "react-router-dom";
+import Home from "./Home";
+import Test from "./Test";
+import Header from "./Header";
 
 function App() {
   return (
-    
     <div className="App">
+      <Header />
       <BrowserRouter>
         <Route exact path="/" component={Home} />
         <Route exact path="/test" component={Test} />
--- client/src/Test.js
+++ client/src/Test.js
@@ -5,65 +5,17 @@ import React from "react";
 function Test() {
   return (
     <div>
-      <div class="jumbotron jumbotron-fluid fixed-top">
-        <div class="container">
-          <h1>UF Student Investment Club</h1>
-          <nav class="navbar navbar-expand-lg navbar-header">
-            <div class="container-fluid">
-              <button
-                class="navbar-toggler"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#navbarNav"
-                aria-controls="navbarNav"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-              >
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                  <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/">
-                      Home
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="/test">
-                      Test
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">
-                      Pricing
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a
-                      class="nav-link disabled"
-                      href="#"
-                      tabindex="-1"
-                      aria-disabled="true"
-                    >
-                      Disabled
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </nav>
-        </div>
-      </div>
+      does stuff
+      <div>does stuff</div>
+      <div>does stuff</div>
+      <div>does stuff</div>
+      <div>does stuff</div>
+      <div>does stuff</div>
+      <div>does stuff</div>
+      <div>does stuff</div>
+      <div>does stuff</div>
     </div>
   );
 }
 
 export default Test;
-{
-  /*<p>This is a paragraph section.
-    sdfhsdjfhgsdhjgfjsdhgfhjsd
-</p>
-<a href="/test" class="btn btn-headers" role="test">To Test</a>
-<button type="button" class="btn btn-headers">header</button>
-<button type="Test" class="btn btn">Go to Test</button>*/
-}
